bdd121cafdfa7935fc70503455f44ca12e225c8b
menesdurak <m.enes.durak@gmail.com>
2022-12-09 22:51:34 +0300

    Gradle upgrades

--- app/src/main/java/com/menesdurak/retrofit_cryptotracker/MainActivity.kt
+++ app/src/main/java/com/menesdurak/retrofit_cryptotracker/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json
     }
 }
