f5de02b524a082c27806cbf90e1a361f7443fd82
menesdurak <m.enes.durak@gmail.com>
2022-12-10 10:37:06 +0300

    Adapter created

---
+++ app/src/main/java/com/menesdurak/retrofit_cryptotracker/adapter/RecyclerViewAdapter.kt
@@ -0,0 +1,44 @@
+package com.menesdurak.retrofit_cryptotracker.adapter
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.menesdurak.retrofit_cryptotracker.R
+import com.menesdurak.retrofit_cryptotracker.model.CryptoModel
+
+class RecyclerViewAdapter(private val cryptoList: ArrayList<CryptoModel>, private val listener: Listener) : RecyclerView.Adapter<RecyclerViewAdapter.RowHolder>() {
+
+    interface Listener {
+        fun onItemClick(cryptoModel: CryptoModel)
+    }
+
+    private val colors: Array<String> = arrayOf("#FAF8F1", "#FAEAB1", "#E5BA73", "#C58940", "#439A97", "#62B6B7")
+
+    class RowHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        fun bind(cryptoModel: CryptoModel, colors: Array<String>, position: Int, listener: Listener) {
+            itemView.setOnClickListener {
+                listener.onItemClick(cryptoModel)
+            }
+            itemView.setBackgroundColor(Color.parseColor(colors[position % 6]))
+            itemView.findViewById<TextView>(R.id.text_name).text = cryptoModel.currency
+            itemView.findViewById<TextView>(R.id.text_price).text = cryptoModel.price
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RowHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_layout, parent, false)
+        return RowHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RowHolder, position: Int) {
+        holder.bind(cryptoList[position], colors, position, listener)
+    }
+
+    override fun getItemCount(): Int {
+        return cryptoList.count()
+    }
+}
